79b2ae749654d511acbcad59b21c0729b2584a24
RealMG <realmg51@gmail.com>
2026-04-15 22:02:24 +0700

    fix: fix typo in comment

--- src/kotlin/normal/ImmutableVariables.kt
+++ src/kotlin/normal/ImmutableVariables.kt
@@ -1,5 +1,5 @@
 fun main()
 {
-  val name: String = "realmg" // imutable variable uses 'val',so you cannot edit or re-cook it
+  val name: String = "realmg" // immutable variable uses 'val',so you cannot edit or re-cook it
   println(name)
 }
