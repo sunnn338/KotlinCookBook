7c0a567484271817d2e8088cf646d1963f571d4b
RealMG <realmg51@gmail.com>
2026-04-16 18:39:59 +0700

    chore: change file name and add comments

--- src/kotlin/normal/IfChef/IfChef.kt
+++ src/kotlin/normal/IfChef/IfChef.kt
@@ -1,9 +1,9 @@
 // Sometimes you need to choose between two actions:if (this) do (it) else do (something else)
 fun main() {
-  val isSalty = true
+  val isSalty = true // you can change it to val/var <anything> = <anything>
   if (isSalty) {
     println ("Oh,too salty,I need to do something")
   } else {
-    println ("well,good job boy!")
+    println ("well,good job boy!") // else  handle
   }
 }
