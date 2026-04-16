9e4256c2ded440726010cbb2b38d7578caded074
RealMG <realmg51@gmail.com>
2026-04-16 13:03:15 +0700

    Delete src/kotlin/normal/MutableVariables.kt

--- src/kotlin/normal/MutableVariables.kt
+++
@@ -1,9 +0,0 @@
-// Muteable variable help you can change ingredients on it
-// let cook to know what is it!
-fun main(){
-  var mutableName = "realmg"
-  println(mutableName)
-  mutableName = "someone"
-  println(mutableName)
-  // DO NOT try to change immutable variable (val)
-}
