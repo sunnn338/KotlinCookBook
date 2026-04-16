fca763f1db7fa961a9e9578f6b7d4a565e55ef05
RealMG <realmg51@gmail.com>
2026-04-16 13:06:28 +0700

    Delete src/kotlin/normal/StringCooking.kt

--- src/kotlin/normal/StringCooking.kt
+++
@@ -1,7 +0,0 @@
-// nấu "string"
-fun main(){
-  val filename = "realmg.kt"
-  val name = filename.substringBefore(".") // expected result: "realmg"
-  val extension = filename.substringAfter(".") // expected result: "kt"
-  println("$filename info: $name, extension: $extension")
-}
