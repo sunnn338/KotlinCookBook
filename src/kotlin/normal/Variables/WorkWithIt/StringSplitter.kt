72cc5bf93854347a71be527a2131ed384600e375
RealMG <realmg51@gmail.com>
2026-04-17 20:19:38 +0700

    refractor: move Work with it dictionary to new home

---
+++ src/kotlin/normal/Variables/WorkWithIt/StringSplitter.kt
@@ -0,0 +1,7 @@
+// let's split the string 
+fun main() {
+  val filename = "realmg.kt"
+  val name = filename.substringBefore(".") // expected result: "realmg"
+  val extension = filename.substringAfter(".") // expected result: "kt"
+  println("$filename info: $name, extension: $extension")
+}
