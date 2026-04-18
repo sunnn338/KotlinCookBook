44853ca111f3f889c9386ac43f8d3e24b6dfcb11
RealMG <realmg51@gmail.com>
2026-04-18 07:47:27 +0700

    style: fix indent

--- src/kotlin/normal/Variables/WorkWithIt/StringSplitter.kt
+++ src/kotlin/normal/Variables/WorkWithIt/StringSplitter.kt
@@ -2,6 +2,6 @@
 fun main() {
     val filename = "realmg.kt"
     val name = filename.substringBefore(".") // expected result: "realmg"
-    val extension = filename.substringAfter(".") // expected result: "kt"
+    val extension = filename.substringAfter(".") // expected result: "kt" 
     println("$filename info: $name, extension: $extension")
 }
