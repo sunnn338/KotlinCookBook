21914558a8c51191b738ec6dea9f41650c298d07
RealMG <realmg51@gmail.com>
2026-04-18 07:30:10 +0700

    style: fix incorrect indent

--- src/kotlin/normal/IfChef/IfChef.kt
+++ src/kotlin/normal/IfChef/IfChef.kt
@@ -2,8 +2,8 @@
 fun main() {
     val isSalty = true // you can change it to val/var <anything> = <anything>
     if (isSalty) {
-        println ("Oh,too salty,I need to do something")
+        println("Oh,too salty,I need to do something")
     } else {
-        println ("well,good job boy!") // else  handle
+        println("well,good job boy!") // else  handle
   }
 }
