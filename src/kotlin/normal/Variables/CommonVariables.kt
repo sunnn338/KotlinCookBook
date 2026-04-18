a688fe815e522f06bb0a3afd147fc69538dd8298
RealMG <realmg51@gmail.com>
2026-04-18 07:31:44 +0700

    style: fix incorrect indent

--- src/kotlin/normal/Variables/CommonVariables.kt
+++ src/kotlin/normal/Variables/CommonVariables.kt
@@ -1,11 +1,11 @@
 fun main() {
-  val number: Int = 6 // Int (number)
-  val character: Char = 'a' // Should always use '',if not? You food will burning!!
-  val word: String = "I like Kotlin!" // Should always use "",'' for single character,"" for string
-  val pi: Float = 3.14f // use 'f' because it's a 32-bit float. To see diffrent with 'Double'
-  val db: Double = 1.677778877 // this is float but 64-bit and not ends with 'f'
+    val number: Int = 6 // Int (number)
+    val character: Char = 'a' // Should always use '',if not? You food will burning!!
+    val word: String = "I like Kotlin!" // Should always use "",'' for single character,"" for string
+    val pi: Float = 3.14f // use 'f' because it's a 32-bit float. To see diffrent with 'Double'
+    val db: Double = 1.677778877 // this is float but 64-bit and not ends with 'f'
   // let's place it to the dish:
-  println ("number: " + number)
-  println ("character: " + character)
+    println("number: " + number)
+    println("character: " + character)
   // and more.
 }
